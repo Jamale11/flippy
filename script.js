5183396eb7bfc956c4ce1e627241d44c85a7b4fb
Jamale11 <100054174+Jamale11@users.noreply.github.com>
2025-09-27 11:50:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ bird.src = "bg.png"; // 👉 ganti "bird.png" dengan gambar kamu, misalnya "me.p
 // Posisi burung
 let bx = 50;
 let by = 150;
-let gravity = 1.0;
+let gravity = 0,6;
 let velocity = 0;
 
 // Pipa
@@ -37,7 +37,7 @@ function draw() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   // Burung
-  ctx.drawImage(bird, bx, by, 40, 40);
+  ctx.drawImage(bird, bx, by, 30, 30);
 
   velocity += gravity;
   by += velocity;
